466cee7655752d1f8fa166064483d57b1205a7e9
USER <USER@DESKTOP-SR30KE3>
2025-11-10 19:45:37 -0500

    asadero

---
+++ src/main/java/com/mycompany/inventarioderestaurante/InventarioDeRestaurante.java
@@ -0,0 +1,8 @@
+package com.mycompany.inventarioderestaurante;
+
+public class InventarioDeRestaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
